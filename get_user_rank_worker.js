6d5386419ba5296e5be0a56fe67af94309b8cf0a
FatMinMin <chiehmin18@gmail.com>
2020-03-22 15:38:37 +0800

    fix: use case in-sensitive search for user ranking history

--- get_user_rank_worker.js
+++ get_user_rank_worker.js
@@ -1,7 +1,7 @@
 onmessage = function(e) {
   let user = e.data[0], contest = e.data[1], startTime = e.data[2], contestData = e.data[3];
   for (let userRank of contestData) {
-    if (userRank.username == user) {
+    if (userRank.username.toLowerCase() == user.toLowerCase()) {
       postMessage({"contest": contest, "startTime": startTime, "rank": userRank.rank, "participants": contestData.length});
     }
   }
