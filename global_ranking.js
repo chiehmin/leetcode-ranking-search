90009624a043a44a7aba6b5649fb0eb9152a8a39
FatMinMin <chiehmin18@gmail.com>
2020-08-15 23:37:58 +0800

    improve performance by using custom client side paging

--- global_ranking.js
+++ global_ranking.js
@@ -5,21 +5,51 @@ var app = new Vue({
     currentPage: 1,
     fields: ['globalRanking', 'username', 'rating', 'country'],
     rank: [],
+    filteredRank: [],
     userFilter: "",
-    totalRows: null,
-    isBusy: true,
+    lastFilter: "All",
+    totalRows: 0,
     countryFilter: "All",
     countryList: ["All"],
   },
   computed: {
-    total: function () {
-      return this.totalRows || this.rank.length;
-    },
     filterTrigger: function () {
       return this.countryFilter + this.userFilter;
     },
   },
   methods: {
+
+    rankProvider: function(ctx) {
+      if (this.rank.length === 0) {
+        return new Promise((resolve, reject) => {
+                      let worker = new Worker("get_global_ranking_worker.js");
+                      worker.onmessage = function(resp) {
+                        console.log("yooo");
+                        resolve(resp);
+                      };
+                      worker.postMessage({});
+                    }).then(function (resp) {
+                      if (!resp.data) {
+                        return [];
+                      }
+                      this.rank = JSON.parse(resp.data.rankingData).data;
+                      this.filteredRank = this.rank;
+                      this.totalRows = this.rank.length;
+                      let start = (this.currentPage - 1) * this.perPage;
+                      return this.rank.slice(start, this.perPage);
+                    }.bind(this));
+      } else {
+        if (this.lastFilter != ctx.filter) {
+          this.currentPage = 1;
+          ctx.currentPage = 1;
+          this.filteredRank = this.rank.filter(this.filterFunc);
+          this.totalRows = this.filteredRank.length;
+          this.lastFilter = ctx.filter;
+        }
+        let start = (ctx.currentPage - 1) * this.perPage;
+        return this.filteredRank.slice(start, start + this.perPage);
+      }
+    },
     loadCountry: function () {
       axios.get('./country.json')
         .then(function (resp) {
@@ -29,14 +59,6 @@ var app = new Vue({
           }
           tmpList.sort();
           this.countryList = this.countryList.concat(tmpList);
-        }.bind(this));
-    },
-    loadData: function () {
-      // loading contest data
-      axios.get('data/global-ranking.json')
-        .then(function (resp) {
-          this.rank = resp.data;
-          this.isBusy = false;
         }.bind(this));
     },
     filterFunc: function (row, _) {
@@ -52,16 +74,11 @@ var app = new Vue({
       return (row.country != null &&
         row.country.toLowerCase() === this.countryFilter.toLowerCase());
     },
-    onFiltered: function (filteredItems) {
-      this.currentPage = 1;
-      this.totalRows = filteredItems.length;
-    },
     profileLink: function (username) {
       return `https://leetcode.com/${username}`;
     },
   },
   mounted: function () {
     this.loadCountry();
-    this.loadData();
   }
 });
