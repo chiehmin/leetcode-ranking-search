b5772e3cd11c87064ff85d35539a12bf4a4aa7e2
Chieh-Min Wang <chiehminw@synology.com>
2021-08-17 10:50:29 +0800

    feat: add contest ranking and global ranking pages

---
+++ src/Contest.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app" class="px-5 py-3">
+    <b-navbar toggleable="lg" type="dark" variant="info" class="mb-2">
+      <b-navbar-brand href="#">{{ unSlugTitle }}</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="index.html">All Contests</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+
+    <a class="github-button" href="https://github.com/chiehmin" data-size="large" data-show-count="true" aria-label="Follow @chiehmin on GitHub">Follow @chiehmin</a>
+    <a class="github-button" href="https://github.com/chiehmin/leetcode-ranking-search" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star chiehmin/leetcode-ranking-search on GitHub">Star</a>
+
+    <b-container fluid class="p-0">
+      <b-row align-h="between">
+        <b-col sm="5">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="total"
+            :per-page="perPage"
+            aria-controls="rank-table"
+            ></b-pagination>
+        </b-col>
+        <b-col sm="3">
+          <b-row align-h="end">
+            <b-form-select
+              v-model="countryFilter"
+              :options="countryList"
+            ></b-form-select>
+          </b-row>
+        </b-col>
+        <b-col sm="4">
+          <b-form-input
+            v-model="userFilter"
+            debounce="500"
+            type="search"
+            placeholder="Search a user..."
+            ></b-form-input>
+        </b-col>
+      </b-row>
+    </b-container>
+
+    <b-table hover
+      id="rank-table"
+      :fields="fields"
+      :items="rank"
+      :per-page="perPage"
+      :current-page="currentPage"
+      :busy="isBusy"
+      :filter="filterTrigger"
+      :filter-function="filterFunc"
+      :filter-included-fields="filteredOn"
+      @filtered="onFiltered"
+      >
+      <template v-slot:table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+      <template v-slot:cell(username)="row">
+        <b-link :href="profileLink(row.value, row.item.data_region)" target="_blank">{{ row.value }}</b-link>
+      </template>
+    </b-table>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return {
+      title: "",
+      perPage: 25,
+      currentPage: 1,
+      fields: ['rank', 'username', 'country_name', 'score', 'finish_time', 'data_region'],
+      rank: [],
+      userFilter: "",
+      filteredOn: ["username", "country_name"],
+      totalRows: null,
+      isBusy: true,
+      countryFilter: "All",
+      countryList: ["All"],
+    };
+  },
+  computed: {
+    total: function () {
+      return this.totalRows || this.rank.length;
+    },
+    unSlugTitle: function () {
+      var words = this.title.split("-");
+      for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+      }
+      return words.join(" ");
+    },
+    filterTrigger: function() {
+      return this.countryFilter + this.userFilter;
+    },
+  },
+  mounted: function () {
+    this.loadCountry();
+    this.loadData();
+  },
+  methods: {
+    loadCountry: function() {
+      axios.get('./country.json')
+        .then(function (resp) {
+          let tmpList = [];
+          for (const [, value] of Object.entries(resp.data)) {
+            tmpList.push(value);
+          }
+          tmpList.sort();
+          this.countryList = this.countryList.concat(tmpList);
+        }.bind(this));
+    },
+    loadData: function () {
+      var contest = new URLSearchParams(new URL(document.URL).search).get("contest");
+      contest = contest ? contest : 'latest';
+
+      // loading contest data
+      axios.get('data/' + contest + '.json')
+        .then(function (resp) {
+          this.isBusy = false;
+          this.rank = resp.data;
+        }.bind(this));
+
+      this.title = contest;
+    },
+    filterFunc: function(row, ) {
+      return this.filterUser(row) && this.filterCountry(row);
+    },
+    filterUser: function (row) {
+      return (row.username.toLowerCase().startsWith(this.userFilter.toLowerCase())) ? true : false;
+    },
+    filterCountry: function(row) {
+      if ("All" === this.countryFilter) {
+        return true;
+      }
+      return (row.country_name != null &&
+        row.country_name.toLowerCase() === this.countryFilter.toLowerCase());
+    },
+    onFiltered: function (filteredItems) {
+      this.currentPage = 1;
+      this.totalRows = filteredItems.length;
+    },
+    profileLink: function(username, data_region) {
+      if ("CN" == data_region) {
+        return `https://leetcode-cn.com/u/${username}`;
+      } else {
+        return `https://leetcode.com/${username}`;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    pages: {
+        index: {
+            entry: "src/main.js",
+            template: "public/index.html",
+            filename: "index.html",
+            title: "Leetcode Ranking Search",
+            chunks: ["chunk-vendors", "chunk-common", "index"]
+        },
+        contest: {
+            entry: "src/contest.js",
+            template: "public/index.html",
+            filename: "contest.html",
+            title: "Contest Ranking",
+            chunks: ["chunk-vendors", "chunk-common", "contest"]
+        },
+        global_ranking: {
+            entry: "src/global_ranking.js",
+            template: "public/index.html",
+            filename: "global_ranking.html",
+            title: "Global Ranking",
+            chunks: ["chunk-vendors", "chunk-common", "global_ranking"]
+        },
+    }
+};
