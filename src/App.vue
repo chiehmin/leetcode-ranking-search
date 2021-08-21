03b7ed61e85ba0521d2c146b9669aa1e747f5bd7
Chieh-Min Wang <chiehminw@synology.com>
2021-08-21 23:55:17 +0800

    chore: change padding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="px-5 pt-3">
+    <div class="px-5 pt-2">
       <a class="github-button" href="https://github.com/chiehmin" data-size="large" data-show-count="true" aria-label="Follow @chiehmin on GitHub">Follow @chiehmin</a>
       <a class="github-button" href="https://github.com/chiehmin/leetcode-ranking-search" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star chiehmin/leetcode-ranking-search on GitHub">Star</a>
     </div>
